6961f8b999f1fc7b90d395a3b8f333e6f24b712a
Sam Maxwell <maxwell.g.sam@gmail.com>
2019-03-09 17:02:50 +0000

    - Implemented basic GUI layout with no background functionality

--- EEESineWaves/EEESineWaves.py
+++ EEESineWaves/EEESineWaves.py
@@ -9,7 +9,7 @@ and the sort of signals that are used to communicate in space.
 #-----------------------------------------------------------------------------------------------------
 #MODULES
 #-----------------------------------------------------------------------------------------------------
-from PyQt5 import QtGui, QtWidgets
+from PyQt5 import QtGui, QtWidgets, QtCore
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
@@ -54,13 +54,28 @@ class mainWindow(QtWidgets.QWidget):
         self.gridLayout = QtWidgets.QGridLayout()
 
         self.graphingCanvas = matplotlibGraph()
-        self.gridLayout.addWidget(self.graphingCanvas, 1, 0, 2, 3)
+        self.gridLayout.addWidget(self.graphingCanvas, 1, 0, 4, 6)
 
         self.graphingToolbar = matplotlibToolbar(self, self.graphingCanvas)
-        self.gridLayout.addWidget(self.graphingToolbar, 0, 0, 1, 3)
+        self.gridLayout.addWidget(self.graphingToolbar, 0, 0, 1, 6)
 
         self.carrierFrequencySlider = slider()
-        self.gridLayout.addWidget(self.carrierFrequencySlider, 0, 4, 3, 1)
+        self.gridLayout.addWidget(self.carrierFrequencySlider, 7, 0, 1, 3)
+
+        self.modulationFrequencySlider = slider()
+        self.gridLayout.addWidget(self.modulationFrequencySlider, 7, 3, 1, 3)
+
+        self.carrierFrequencyNumber = label("0.0")
+        self.gridLayout.addWidget(self.carrierFrequencyNumber, 6, 0, 1, 3)
+
+        self.modulationFrequencyNumber = label("0.0")
+        self.gridLayout.addWidget(self.modulationFrequencyNumber, 6, 3, 1, 3)
+
+        self.carrierFrequencyLabel = label("Carrier Frequency:")
+        self.gridLayout.addWidget(self.carrierFrequencyLabel, 5, 0, 1, 3)
+
+        self.modulationFrequencyLabel = label("Modulation Frequency:")
+        self.gridLayout.addWidget(self.modulationFrequencyLabel, 5, 3, 1, 3)
 
         #Adding the Grid Layout to the Window Layout
         self.setLayout(self.gridLayout)
@@ -92,7 +107,8 @@ class matplotlibGraph(FigureCanvas):
         super(matplotlibGraph, self).__init__(self.figure)
         
         #Generating an Axes to Plot on
-        self.ax = self.figure.add_subplot(111)
+        self.ax = self.figure.add_subplot(211)
+        self.ax1 = self.figure.add_subplot(212)
     
     #Method used to plot a line graph of data
     def linePlot(self, dataX, dataY):
@@ -115,13 +131,40 @@ class slider(QtWidgets.QSlider):
     #Constructor
     def __init__(self):
         #Inherting from the QtWidgets Slider Class
-        super(slider, self).__init__()
+        super(slider, self).__init__(QtCore.Qt.Horizontal)
 
         #Setting the attributes of the Slider
         self.setMinimum = self.minimumVal
         self.setMaximum = self.maximumVal
         self.TickPosition = self.NoTicks
-    
+
+#A widget for GUI text displays
+class label(QtWidgets.QLabel):
+    #Definitions
+
+    #Constructor
+    def __init__(self, labelText):
+        #Inheriting from the QtWidgets Label Class
+        super(label, self).__init__(labelText)
+
+        #Setting the attributes of the label
+        self.newFont = font()
+        self.setFont(self.newFont)
+
+#A widget for text fonts
+class font(QtGui.QFont):
+    #Definitions
+    fontSize = 20
+
+    #Constructor
+    def __init__(self):
+        #Inheriting from the QtGui Font Class
+        super(font, self).__init__()
+        
+        #Setting the attributes of the font
+        self.setBold(True)
+        self.setPointSize(self.fontSize)
+        self.setFamily("Calibri")
 #-----------------------------------------------------------------------------------------------------
 #MAIN CODE
 #-----------------------------------------------------------------------------------------------------
